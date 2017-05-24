c3294e0e863df0d4bdfb9f5d38ef80e7a6c7fd93
patrilopez17 <patricia.lopemo@gmail.com>
2017-05-24 15:04:54 +0200

    Conflicto 2 p52

--- seeders/20170323213930-FillQuizzesTable.js
+++ seeders/20170323213930-FillQuizzesTable.js
@@ -7,28 +7,24 @@ module.exports = {
             {
                 question: 'Capital de Italia',
                 answer: 'Roma',
-		score: 0,
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
                 question: 'Capital de Portugal',
                 answer: 'Lisboa',
-		score: 0,
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
                 question: 'Capital de España',
                 answer: 'Madrid',
-		score: 0,
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
             {
                 question: 'Capital de Francia',
                 answer: 'París',
-		score: 0,
                 createdAt: new Date(),
                 updatedAt: new Date()
             }
