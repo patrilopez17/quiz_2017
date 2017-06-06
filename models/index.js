0b604f522b669a7703edbd402edb8373f98a083b
patrilopez17 <patricia.lopemo@gmail.com>
2017-06-06 11:51:23 +0200

    cmcvn

--- models/index.js
+++ models/index.js
@@ -26,5 +26,19 @@ var sequelize = new Sequelize(url, {storage: storage});
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+// Create and initiate table 
+sequelize.sync().then(function() {
+  Quiz.count().then(function(count) {
+    if(count === 0) {
+      Quiz.create({
+        question: 'Capital de Italia',
+        answer: 'Roma'
+      }).then(function() {
+        console.log('Quizzes table initialized with data');
+      });
+    }
+  })
+});
+
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
